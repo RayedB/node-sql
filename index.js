1f0810b1c4954856a10438fab609b2fa8a7df251
RayedB <rayed.benbrahim@gmail.com>
2018-03-09 21:36:28 +0100

    completed backend

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require('express');
 const mysql = require('mysql');
 const sqlrequire = require('require-sql');
 const createDB = require('./assets/sql/createDB.sql')
+const selectDB = require('./assets/sql/selectDB.sql')
 const createTable = require('./assets/sql/createTable.sql')
 const populate = require('./assets/sql/populateTable.sql')
+const select = require('./assets/sql/select.sql')
 const app = express();
 const conn = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : '',
-  multipleStatements: true
+  password : ''
 });
 
 
@@ -19,19 +20,20 @@ conn.connect(function(){
 
 conn.query('SHOW DATABASES;',function(err,res,fields){
   if (!res.find(db => db.Database == "masterdb")) {
-    conn.query(createDB, function() {
-      conn.query(createTable, function(){
-        conn.query(populate,function(){
-          console.log("you're all set")
-        })
-      })
-    })
+    conn.query(createDB);
+    conn.query(selectDB);
+    conn.query(createTable);
+    conn.query(populate);
   } else {
-
+    conn.query(selectDB);
   }
 
 })
 
-app.get('/api/sql/requete1')
+app.get('/api/sql/requete1', function(request,response){
+  conn.query(select, function (err,res,fields){
+    response.json(res)
+  });
+})
 
 app.listen(8080);
